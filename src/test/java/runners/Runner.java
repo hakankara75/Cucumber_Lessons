fa9bd4153ea4336ff2f564ef102b453018a63c2a
hakankara75 <hakanbatirhan@gmail.com>
2023-12-22 14:46:24 +0300

    v

--- src/test/java/runners/Runner.java
+++ src/test/java/runners/Runner.java
@@ -25,7 +25,7 @@ senaryoyu çalıştırabiliriz
         /* 1-Bu parametre ile kodlarımızı yazdığımız stepDefinition class'ının packege'ını belirtiriz
 2- Hooks classin yolunu veririz.
                                              */
-                 tags = "@login",
+                 tags = "@yemek",
                  dryRun = false,
                  monochrome = false //console okunakli hale getirir,false scenario adimlarini renkli gosterir,  true renksiz yapar
                         )
---
+++ src/test/java/utilities/ConReader.java
@@ -0,0 +1,36 @@
+package utilities;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+public class ConReader {
+
+    //Önce properties dosyasındaki verileri okuyabilmek için java'dan properties class'ından bir obje oluştururuz
+    public static Properties properties;
+
+    //Herşeyden önce çalışması için static bloc içerisinde, oluşturmuş olduğum properties dosyasını tanımlar
+    //ve atamasını yaparız. FileInputStream ile dosya yolunu akışa alırız.
+
+    static {
+        String dosyaYolu = "configuration.properties";
+        try {
+            FileInputStream fis = new FileInputStream(dosyaYolu);
+            properties = new Properties(); //objeyi oluşturduk ve atamasını gerçekleştirdik
+            properties.load(fis);//fis'in okuduğu bilgileri properties'e yükler
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    //properties dosyasındaki key değerini alıp test methodumda kullanabilmek için bir method oluşturmalıyız
+    public static String getProperty(String Key){//String bir değer döndürmesi için String bir parametre atarız
+        /*
+            Test method'undan gönderdiğimiz string key değerini alıp ConfigReader class'ından
+        getProperty() methodunu kullanarak bu key'e ait value'yu bize getirir.
+         */
+
+
+        return properties.getProperty(Key);
+    }
+
+}
